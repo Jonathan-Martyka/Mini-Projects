105f14720d1a06d45952a9db5386dbdbd71637a3
Jonathan Martyka <jonathanmartyka@gmail.com>
2020-08-19 03:17:19 -0500

    NumToRom Input
    Added console input and echo until specified

--- Numeric To Roman Numeral Converter/src/NumToRomConverter.java
+++ Numeric To Roman Numeral Converter/src/NumToRomConverter.java
@@ -1,4 +1,24 @@
+import java.util.Scanner;
 
 public class NumToRomConverter {
-
+	public static void main(String args[]) {
+		System.out.println("Welcome to the Numeric to Roman Numeral Converter"
+				+ "\nEnter a blank line to quit at any time");
+		System.out.println("Please enter an integer");
+		
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		while (input != null && !input.contentEquals("")) {
+			System.out.println(input + " -> " + "II");
+			System.out.println("Please enter an integer");
+			input = scanner.nextLine();
+		}
+		System.out.println("Exiting... Goodbye");
+		
+	}
+	
+	private String numericToRoman(int input) {
+		
+		return "";
+	}
 }
