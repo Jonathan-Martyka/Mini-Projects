557632be7cd70305e5ffa9be3883a825909c2791
Jonathan Martyka <jonathanmartyka@gmail.com>
2020-08-19 03:43:41 -0500

    Added Roman Numeral
    Added functions returning correct Roman numerals

--- Numeric To Roman Numeral Converter/src/NumToRomConverter.java
+++ Numeric To Roman Numeral Converter/src/NumToRomConverter.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class NumToRomConverter {
+	
 	public static void main(String args[]) {
 		System.out.println("Welcome to the Numeric to Roman Numeral Converter"
 				+ "\nEnter a blank line to quit at any time");
@@ -17,8 +18,62 @@ public class NumToRomConverter {
 		
 	}
 	
-	private String numericToRoman(int input) {
+	private String numericToRoman(int number) {
 		
 		return "";
+	}
+	
+	private String onesToRoman(int number) {
+		switch (number) {
+		case 1: return "I";
+		case 2: return "II";
+		case 3: return "III";
+		case 4: return "IV";
+		case 5: return "V";
+		case 6: return "VI";
+		case 7: return "VII";
+		case 8: return "VIII";
+		case 9: return "IX";
+		default: return "";
+		}
+	}
+	
+	private String tensToRoman(int number) {
+		switch (number) {
+		case 1: return "X";
+		case 2: return "XX";
+		case 3: return "XXX";
+		case 4: return "XL";
+		case 5: return "L";
+		case 6: return "LX";
+		case 7: return "LXX";
+		case 8: return "LXXX";
+		case 9: return "LC";
+		default: return "";
+		}
+	}
+	
+	private String hundredsToRoman(int number) {
+		switch (number) {
+		case 1: return "C";
+		case 2: return "CC";
+		case 3: return "CCC";
+		case 4: return "CD";
+		case 5: return "D";
+		case 6: return "DC";
+		case 7: return "DCC";
+		case 8: return "DCCC";
+		case 9: return "CM";
+		default: return "";
+		}
+	}
+	private String thousandsToRoman(int number) {
+		//Note: Roman numerals cap at 3999
+		switch (number) {
+		case 1: return "M";
+		case 2: return "MM";
+		case 3: return "MMM";
+		default: return "";
+		}
 	}
 }
